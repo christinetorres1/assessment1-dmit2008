6275304d3dcab287d03231ce97d75605673ab9d9
Christine Torres <christinejoytorres1@outlook.com>
2022-02-08 14:05:00 -0800

    write done

--- src/js/libs/firebase/firebaseConfig.js
+++ src/js/libs/firebase/firebaseConfig.js
@@ -9,20 +9,20 @@ import {getStorage} from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAIbHRkz3KA2UGcOCWzJCfbd1ZBefHuGss",
-  authDomain: "chocolatestore-f86cb.firebaseapp.com",
-  databaseURL: "https://chocolatestore-f86cb-default-rtdb.firebaseio.com",
-  projectId: "chocolatestore-f86cb",
-  storageBucket: "chocolatestore-f86cb.appspot.com",
-  messagingSenderId: "348373430667",
-  appId: "1:348373430667:web:74aacb5f4105599f1fbf8e",
-  measurementId: "G-SZVKQHH1TX"
+  apiKey: "AIzaSyAWX1c4Oh0ASGtbhO7C2JqsYUR5GCEPNgI",
+  authDomain: "storefront-79639.firebaseapp.com",
+  databaseURL: "https://storefront-79639-default-rtdb.firebaseio.com",
+  projectId: "storefront-79639",
+  storageBucket: "storefront-79639.appspot.com",
+  messagingSenderId: "486486536022",
+  appId: "1:486486536022:web:e9465daf9e52fd60d130f7",
+  measurementId: "G-DYQ0S4BW5Q"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const storage = getStorage(app)
+const storage = getStorage(app);
 
 
 export{ db, storage }
