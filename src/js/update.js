1807c9961fd8515d30b8893ba610bfe01ed312c2
Christine Torres <christinejoytorres1@outlook.com>
2022-02-08 17:06:40 -0800

    a

--- src/js/update.js
+++ src/js/update.js
@@ -3,20 +3,58 @@ import {ref as databaseRef, set, get} from 'firebase/database'
 import { db, storage  } from "./libs/firebase/firebaseConfig";
 
 
-function pageInit () {
+const chocolateForm = document.forms['chocolateForm']
+
+async function pageInit () {
     const key = sessionStorage.getItem('key');
     const chocolateRef = databaseRef(db, `chocolates/${key}`)
     const chocolateSnapShot = await get(chocolateRef)
-    const chocolate = chocolateSnapShot.val()
+    // const chocolate = chocolateSnapShot.val()
 
-    if(chocolate.exists())
-    {
+    if(chocolateSnapShot.exists()) {
         setFieldValues(chocolateSnapShot.val())        
     }
 
+    chocolateForm.addEventListener('submit', onUpdateChocolate)
+
+}
+
+function onUpdateChocolate(e) {
+    e.preventDefault();
+    // const name = e.target.elements['chocolateName'].value.trim();
+    // const price = e.target.elements['chocolatePrice'].value.trim();
+    // const pieces = e.target.elements['chocolatePieces'].value.trim();
+    // const file = e.target.elements['chocolateImage'].files
+
+    updateChocolateData();
 }
 
 
-function setFieldValues(chocolate) {
-    const chocolateForm = document
+function setFieldValues({name, price, pieces, urlPath}) {
+    const name = chocolateForm.elements['chocolateName'].value
+    const urlPath = document.querySelector('#uploadImage').src
 }
+
+function updateChocolateData(){
+    const name = chocolateForm.elements['chocolateName'].value.trim();
+    const price = chocolateForm.elements['chocolatePrice'].value.trim();
+    const pieces = chocolateForm.elements['chocolatePieces'].value.trim();
+    const file = chocolateForm.elements['chocolateImage'].files
+
+    if(file.length !== 0) {
+        const imageRef = storageRef(storage, `images/${file.name}`)
+        
+    }
+
+    const key = sessionStorage.getItem('key');
+    const dataRef = databaseRef(db, `chocolates/${key}`)
+
+    set (dataRef, {
+        urlPath,
+        name,
+        price,
+        pieces
+    })
+}
+
+pageInit()
